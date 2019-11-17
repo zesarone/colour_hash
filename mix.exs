fe98dab6a4a2013cdff6d468f1c5d66ba97a9412
Emil <emil.hammarstedt@gmail.com>
2019-11-17 23:41:17 +0100

    added credo to check code standard

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule ColourHash.MixProject do
 
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
